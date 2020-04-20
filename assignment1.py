59dfde4860f8b7ac398ad6184263b32edb9ecbf4
raheelchagani <63993832+raheelchagani@users.noreply.github.com>
2020-04-20 08:48:07 +0400

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,6 @@
+num1 = input ("Enter Number 1: ")
+num2 = input("Enter Number 2: ")
+print(int(num1) + int(num2))
+print(int(num1) - int(num2))
+print(int(num1) / int(num2))
+print(int(num1) * int(num2))
